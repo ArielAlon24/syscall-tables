f41da4383c6ae991c5652f46ae4c655bb68b3bbe
Ariel Alon <arielalon.12321@gmail.com>
2024-08-24 17:00:05 +0300

    Fixed router issues

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -39,4 +40,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'public'), to: path.resolve(__dirname, 'dist') },
+      ],
+    }),
+  ],
 };
